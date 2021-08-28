ec00f95095895a1b9a15d25935dac9b12af040a0
Aaron Puente <AaronBacon101@gmail.com>
2021-08-27 21:50:32 -0500

    aug27

--- src/Pages/Homepage.vue
+++ src/Pages/Homepage.vue
@@ -6,7 +6,11 @@
 <router-link class='homepage_text' to='/calculus'>Calculus</router-link><br>
 <router-link class='homepage_text' to='/stats'>Stats</router-link><br>
 <router-link class='homepage_text' to='/vectors'>Vectors</router-link><br>
-<router-link class='homepage_text' to='/linear_algebra'>Linear Algebra</router-link>
+<router-link class='homepage_text' to='/linear_algebra'>Linear Algebra</router-link><br><br><br><br>
+<router-link class='homepage_text_resource' to='https://www.desmos.com/calculator' target='_blank'>Desmos Graphing Calculator</router-link><br>
+<router-link class='homepage_text_resource' to='https://www.desmos.com/scientific' target='_blank'>Desmos Scientific Calculator</router-link><br>
+<router-link class='homepage_text_resource' to='https://www.wolframalpha.com/input/?i=integral' target='_blank'>Antiderivative Calculator</router-link><br>
+<router-link class='homepage_text_resource' to='https://www.wolframalpha.com/input/?i=definite+integral' target='_blank'>Definite Integral Calculator</router-link><br>
 <router-view/>
 </template>
 
@@ -30,5 +34,16 @@
   font-size: 28px;
   color: black;
 }
-
+.homepage_text_resource{
+  font: Times;
+  font-size: 28px;
+  text-decoration: none;
+  color :green;
+}
+.homepage_text_resource:visited{
+  font: Times;
+  text-decoration: none;
+  font-size: 28px;
+  color: green;
+}
 </style>
